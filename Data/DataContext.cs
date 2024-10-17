8f4a3dbc846fb62db9ecb2fe6e4f470513c4a704
Ly <71586774+WannaCry081@users.noreply.github.com>
2024-10-17 16:15:01 +0800

    chore: add data context

---
+++ Data/DataContext.cs
@@ -0,0 +1,9 @@
+using Microsoft.EntityFrameworkCore;
+using sample_auth_aspnet.Models.Entities;
+
+namespace sample_auth_aspnet.Data;
+
+public partial class DataContext(DbContextOptions<DataContext> options) : DbContext(options)
+{
+    public DbSet<User> Users { get; set; }
+}
