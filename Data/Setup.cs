8ac656486d5b47ca3a363bcb5c85fbdaaecb71c0
Ly <71586774+WannaCry081@users.noreply.github.com>
2024-10-17 16:15:24 +0800

    chore: add data setup

---
+++ Data/Setup.cs
@@ -0,0 +1,20 @@
+using Microsoft.EntityFrameworkCore;
+using sample_auth_aspnet.Models.Entities;
+
+namespace sample_auth_aspnet.Data;
+
+public partial class DataContext
+{
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<User>()
+            .HasIndex(u => new
+            {
+                u.Email,
+                u.UserName
+            })
+            .IsUnique();
+        base.OnModelCreating(modelBuilder);
+    }
+}
+
