9bd12d655ccc26966ef3732ac8d8e7f7679ae175
Ly <71586774+WannaCry081@users.noreply.github.com>
2024-10-23 01:29:37 +0800

    ref: add data annotation in auth login dto

--- Models/Dtos/Auth/AuthLoginDto.cs
+++ Models/Dtos/Auth/AuthLoginDto.cs
@@ -1,3 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace sample_auth_aspnet.Models.Dtos.Auth;
 
-public record AuthLoginDto(string Email, string Password);
+public class AuthLoginDto
+{
+    [EmailAddress(ErrorMessage = "Invalid Email Address")]
+    public string Email { get; init; } = string.Empty;
+
+    [Required(ErrorMessage = "Password is required")]
+    public string Password { get; init; } = string.Empty;
+};
