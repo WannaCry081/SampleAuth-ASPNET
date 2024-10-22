776ebaf284f875df03334ad43a60a3e62d469606
Ly <71586774+WannaCry081@users.noreply.github.com>
2024-10-23 01:58:00 +0800

    ref: update  id data type to guid

--- Models/Entities/BaseEntity.cs
+++ Models/Entities/BaseEntity.cs
@@ -5,7 +5,7 @@ namespace sample_auth_aspnet.Models.Entities;
 public class BaseEntity
 {
     [Key]
-    public int Id { get; set; }
+    public Guid Id { get; set; }
     public DateTime CreatedAt { get; set; }
     public DateTime? UpdatedAt { get; set; }
 }
