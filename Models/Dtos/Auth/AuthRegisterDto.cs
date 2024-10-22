772358aef7f9ec8a75d606b13b674f8df5b801ef
Ly <71586774+WannaCry081@users.noreply.github.com>
2024-10-23 01:29:53 +0800

    ref: add data annotation in auth register dto

--- Models/Dtos/Auth/AuthRegisterDto.cs
+++ Models/Dtos/Auth/AuthRegisterDto.cs
@@ -1,9 +1,23 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace sample_auth_aspnet.Models.Dtos.Auth;
 
-public record AuthRegisterDto(
-    string Email,
-    string FirstName,
-    string LastName,
-    string Password,
-    string RePassword
-);
+public class AuthRegisterDto
+{
+    [EmailAddress(ErrorMessage = "Invalid Email Address")]
+    public string Email { get; init; } = string.Empty;
+
+    [MaxLength(50)]
+    [Required(ErrorMessage = "First Name is required")]
+    public string FirstName { get; init; } = string.Empty;
+
+    [MaxLength(50)]
+    [Required(ErrorMessage = "Last Name is required")]
+    public string LastName { get; init; } = string.Empty;
+
+    [Required(ErrorMessage = "Password is required")]
+    public string Password { get; init; } = string.Empty;
+
+    [Required(ErrorMessage = "Re-Password is required")]
+    public string RePassword { get; init; } = string.Empty;
+}
