345e72dde637bbb8c39f2a1460f0fc3d3b4abe04
Ly <71586774+WannaCry081@users.noreply.github.com>
2024-10-23 01:12:44 +0800

    feat: create generate jwt token method

---
+++ Services/Utils/Token.cs
@@ -0,0 +1,40 @@
+using System.Text;
+using System.Security.Claims;
+using Microsoft.IdentityModel.Tokens;
+using sample_auth_aspnet.Models.Entities;
+using System.IdentityModel.Tokens.Jwt;
+
+namespace sample_auth_aspnet.Services.Utils;
+
+public static class Token
+{
+    private static string GenerateToken(User user, DateTime expires, IConfiguration configuration, bool isRefreshToken = false)
+    {
+        var claims = new List<Claim>
+        {
+            new(JwtRegisteredClaimNames.Sub, user.Id.ToString()),
+            new(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+            new(JwtRegisteredClaimNames.Iat, new DateTimeOffset(DateTime.UtcNow).ToUnixTimeSeconds().ToString(), ClaimValueTypes.Integer64)
+        };
+
+        if (!isRefreshToken)
+        {
+            claims.Add(new Claim(ClaimTypes.Email, user.Email));
+            claims.Add(new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()));
+        }
+
+        var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["JWT:Key"]!));
+        var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+        var token = new JwtSecurityToken(
+            issuer: configuration["JWT:Issuer"],
+            audience: configuration["JWT:Audience"],
+            claims: claims,
+            expires: expires,
+            signingCredentials: creds
+        );
+
+        return new JwtSecurityTokenHandler().WriteToken(token);
+    }
+}
+
