8b55b54398b0733df17a74ae69a285f066cab8a3
Ly <71586774+WannaCry081@users.noreply.github.com>
2024-10-23 01:21:44 +0800

    chore: create get action result from error method

---
+++ Controllers/Utils/ControllerUtil.cs
@@ -0,0 +1,23 @@
+using Microsoft.AspNetCore.Mvc;
+using sample_auth_aspnet.Models.Response;
+using static sample_auth_aspnet.Models.Utils.Errors;
+
+namespace sample_auth_aspnet.Controllers.Utils;
+
+public static class ControllerUtil
+{
+    public static IActionResult GetActionResultFromError<T>(ApiResponse<T> apiResponse)
+    {
+        var errorType = apiResponse.ErrorType;
+
+        return errorType switch
+        {
+            ErrorType.NotFound => new NotFoundObjectResult(apiResponse),
+            ErrorType.BadRequest => new BadRequestObjectResult(apiResponse),
+            ErrorType.Unauthorized => new UnauthorizedResult(),
+            ErrorType.ValidationError => new BadRequestObjectResult(apiResponse),
+            ErrorType.InternalServerError => new StatusCodeResult(500),
+            _ => new BadRequestObjectResult(apiResponse)
+        };
+    }
+}
