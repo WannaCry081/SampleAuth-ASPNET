b56fb28f79104e2eb84440954dcb676108c77393
Ly <71586774+WannaCry081@users.noreply.github.com>
2024-10-23 01:16:34 +0800

    chore: update register user service implementation

---
+++ Services/Auth/AuthService.cs
@@ -0,0 +1,50 @@
+using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using sample_auth_aspnet.Data;
+using sample_auth_aspnet.Models.Dtos.Auth;
+using sample_auth_aspnet.Models.Entities;
+using sample_auth_aspnet.Models.Response;
+using sample_auth_aspnet.Models.Utils;
+using sample_auth_aspnet.Services.Utils;
+
+namespace sample_auth_aspnet.Services.Auth;
+
+public class AuthService(
+    DataContext context,
+    IMapper mapper,
+    IConfiguration configuration
+) : IAuthService
+{
+    public async Task<ApiResponse<AuthDto>> RegisterUserAsync(AuthRegisterDto authRegister)
+    {
+        if (!authRegister.Password.Equals(authRegister.RePassword))
+        {
+            return ApiResponse<AuthDto>.ErrorResponse(
+                Errors.Unauthorized, Errors.ErrorType.BadRequest);
+        }
+
+        var isUserExists = await context.Users.FirstOrDefaultAsync(
+            u => u.Email.Equals(authRegister.Email)
+        );
+
+        if (isUserExists != null)
+        {
+            return ApiResponse<AuthDto>.ErrorResponse(
+                $"User {Errors.AlreadyExists.ToLower()}", Errors.ErrorType.BadRequest);
+        }
+
+        var user = mapper.Map<User>(authRegister);
+        user.Password = BCrypt.Net.BCrypt.HashPassword(user.Password);
+
+        await context.Users.AddAsync(user);
+        await context.SaveChangesAsync();
+
+        return ApiResponse<AuthDto>.SuccessResponse(
+            new AuthDto
+            {
+                Access = Token.GenerateAccess(user, configuration),
+                Refresh = Token.GenerateRefresh(user, configuration)
+            }
+        );
+    }
+}
