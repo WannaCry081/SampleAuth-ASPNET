968b9d9466e58379ee70f7c24554e57877561a2c
Ly <liraedata59@gmail.com>
2024-10-24 15:22:51 +0800

    docs: add swagger doc to auth login dto

--- Models/Dtos/Auth/AuthLoginDto.cs
+++ Models/Dtos/Auth/AuthLoginDto.cs
@@ -1,12 +1,19 @@
-using System.ComponentModel.DataAnnotations;
-
 namespace sample_auth_aspnet.Models.Dtos.Auth;
 
+/// <summary>
+/// DTO used for login a user.
+/// </summary>
 public class AuthLoginDto
 {
-    [EmailAddress(ErrorMessage = "Invalid Email Address")]
+    /// <summary>
+    /// The email address of the user.
+    /// </summary>
+    /// <example>user@example.com</example>
     public string Email { get; init; } = string.Empty;
 
-    [Required(ErrorMessage = "Password is required")]
+    /// <summary>
+    /// The password for the account.
+    /// </summary>
+    /// <example>StrongPassword123</example>
     public string Password { get; init; } = string.Empty;
 };
