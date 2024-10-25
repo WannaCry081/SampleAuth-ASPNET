66d745ab010dbcbd9ecc14bbb0dd19a43b67c14a
Ly <liraedata59@gmail.com>
2024-10-26 03:05:13 +0800

    chore: add success user_is_authenticated method

---
+++ Models/Utils/Success.cs
@@ -0,0 +1,9 @@
+namespace sample_auth_aspnet.Models.Utils;
+
+public static class Success
+{
+    public static string USER_IS_AUTHENTICATED()
+    {
+        return "User has been successfully logged in";
+    }
+}
