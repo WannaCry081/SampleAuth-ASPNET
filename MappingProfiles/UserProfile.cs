c8328bd67191a79fafc76b72c5c619b7bff92d56
Ly <liraedata59@gmail.com>
2024-10-26 16:57:34 +0800

    docs: add xml comments in user profile

--- MappingProfiles/UserProfile.cs
+++ MappingProfiles/UserProfile.cs
@@ -4,8 +4,14 @@ using sample_auth_aspnet.Models.Entities;
 
 namespace sample_auth_aspnet.MappingProfiles;
 
+/// <summary>
+/// AutoMapper profile for user-related mappings
+/// </summary>
 public class UserProfile : Profile
 {
+    /// <summary>
+    /// Initializes the mapping configuration for authentication DTOs and entities.
+    /// </summary>
     public UserProfile()
     {
         CreateMap<User, UserDetailsDto>();
