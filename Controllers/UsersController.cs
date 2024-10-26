9ce434126837a2d29c479b434b06e3ba0a105ea3
Ly <liraedata59@gmail.com>
2024-10-26 16:57:48 +0800

    docs: add swagger docs in user controller

--- Controllers/UsersController.cs
+++ Controllers/UsersController.cs
@@ -5,6 +5,9 @@ using sample_auth_aspnet.Services.Users;
 
 namespace sample_auth_aspnet.Controllers;
 
+/// <summary>
+/// Controller for handling authenticated users.
+/// </summary>
 [ApiController]
 [ApiVersion("1.0")]
 [Route("api/v{version:apiVersion}/users")]
@@ -12,6 +15,13 @@ public class UsersController(
     ILogger<UsersController> logger,
     IUserService userService) : ControllerBase
 {
+    /// <summary>
+    /// Fetches the authenticated user's details.
+    /// </summary>
+    /// <returns>An <see cref="IActionResult"/> indicating the result of retrieving user's details.</returns>
+    /// <response code="200">Returns if fetching user's details was successful.</response>
+    /// <response code="401">Returns if the model is not authenticated.</response>
+    /// <response code="500">Returns if an internal server error occurred.</response>
     [Authorize]
     [HttpGet("me")]
     public async Task<IActionResult> GetUserDetails()
