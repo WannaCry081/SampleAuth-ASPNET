5c281cb0b4d5a63de82fb23d508fa99e161ae582
Ly <liraedata59@gmail.com>
2024-10-26 16:28:55 +0800

    chore: create user details dto

---
+++ Models/Dtos/Users/UserDetailsDto.cs
@@ -0,0 +1,8 @@
+namespace sample_auth_aspnet.Models.Dtos.Users;
+
+public class UserDetailsDto
+{
+    public string Email { get; init; } = string.Empty;
+    public string FirstName { get; init; } = string.Empty;
+    public string LastName { get; init; } = string.Empty;
+}
