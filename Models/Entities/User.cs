b67f9fddc655fdefd7260c7c090ab665d767f92f
Ly <liraedata59@gmail.com>
2024-10-27 00:05:15 +0800

    chore: add token property as navigation property

--- Models/Entities/User.cs
+++ Models/Entities/User.cs
@@ -5,9 +5,11 @@ namespace sample_auth_aspnet.Models.Entities;
 [Index(nameof(Email), IsUnique = true)]
 public sealed class User : BaseEntity
 {
-    public Guid UserId { get; init; } = Guid.NewGuid();
     public string Email { get; set; } = null!;
     public string? FirstName { get; set; }
     public string? LastName { get; set; }
     public string Password { get; set; } = null!;
+
+    // Navigation Properties
+    public ICollection<Token> Tokens { get; set; } = [];
 }
