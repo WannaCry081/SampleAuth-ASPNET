8dd2b6f4052704b7c17c33027e99f68726619972
Ly <liraedata59@gmail.com>
2024-10-26 16:58:32 +0800

    docs: add swagger docs in user service

--- Services/Users/UserService.cs
+++ Services/Users/UserService.cs
@@ -7,10 +7,18 @@ using sample_auth_aspnet.Models.Utils;
 
 namespace sample_auth_aspnet.Services.Users;
 
+/// <summary>
+/// Service interface for authenticated user operation
+/// </summary>
 public class UserService(
     DataContext context,
     IMapper mapper) : IUserService
 {
+    /// <summary>
+    /// Fetches the authenticated user's details.
+    /// </summary>
+    /// <param name="userId">The user ID from the access token</param>
+    /// <returns>The user's details of the authenticated users</returns>
     public async Task<ApiResponse<UserDetailsDto>> GetUserDetailsAsync(int userId)
     {
         var user = await context.Users.FirstOrDefaultAsync(
