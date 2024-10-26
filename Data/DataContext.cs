365d2b53e1d19ce349e12ab875e0cbbb632a6813
Ly <liraedata59@gmail.com>
2024-10-27 00:05:38 +0800

    chore: register token entity in data context

--- Data/DataContext.cs
+++ Data/DataContext.cs
@@ -6,4 +6,5 @@ namespace sample_auth_aspnet.Data;
 public partial class DataContext(DbContextOptions<DataContext> options) : DbContext(options)
 {
     public DbSet<User> Users { get; set; }
+    public DbSet<Token> Tokens { get; set; }
 }
