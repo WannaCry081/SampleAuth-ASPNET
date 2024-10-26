5d2b8ccab4c47c5fdaff46b800a60176348be124
Ly <liraedata59@gmail.com>
2024-10-26 11:50:37 +0800

    docs: add xml comments in success response dto

--- Models/Dtos/Reponse/SuccessResponseDto.cs
+++ Models/Dtos/Reponse/SuccessResponseDto.cs
@@ -1,9 +1,23 @@
-
 namespace sample_auth_aspnet.Models.Dtos.Reponse;
 
+/// <summary>
+/// DTO used for wrapping successful response data.
+/// </summary>
+/// <typeparam name="T">The type of data included in the success response.</typeparam>
 public class SuccessResponseDto<T>
 {
-    public string Status { get; init; } = string.Empty;
+    /// <summary>
+    /// Indicates the status of the response, typically set to "success".
+    /// </summary>
+    public string Status { get; init; } = "success";
+
+    /// <summary>
+    /// Message providing details about the success of the operation.
+    /// </summary>
     public string Message { get; init; } = string.Empty;
+
+    /// <summary>
+    /// The data payload associated with the successful response.
+    /// </summary>
     public T? Data { get; init; }
 }
