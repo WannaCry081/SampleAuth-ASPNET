4590ca2146dee421376ed9bbee0520a18efcc9ee
Ly <liraedata59@gmail.com>
2024-10-29 01:12:13 +0800

    chore: add update token property migration

---
+++ Migrations/20241028162650_UpdateTokenProperty.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace sample_auth_aspnet.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateTokenProperty : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "JTI",
+                table: "Tokens",
+                newName: "Refresh");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Tokens_JTI",
+                table: "Tokens",
+                newName: "IX_Tokens_Refresh");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Refresh",
+                table: "Tokens",
+                newName: "JTI");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Tokens_Refresh",
+                table: "Tokens",
+                newName: "IX_Tokens_JTI");
+        }
+    }
+}
