40818943f12980c41da128c81398fdf5558549d5
Ly <liraedata59@gmail.com>
2024-10-30 01:20:26 +0800

    docs: update 'GetUserDetailsAsync' swagger docs

--- Services/Users/IUserService.cs
+++ Services/Users/IUserService.cs
@@ -6,9 +6,11 @@ namespace sample_auth_aspnet.Services.Users;
 public interface IUserService
 {
     /// <summary>
-    /// Fetches the authenticated user's details.
+    ///     Fetches the authenticated user's details.
     /// </summary>
-    /// <param name="id">The user ID from the access token</param>
-    /// <returns>The user's details of the authenticated users</returns>
+    /// <param name="id"></param>
+    /// <returns>
+    ///     The user's details of the authenticated users
+    /// </returns>
     Task<ApiResponse<UserDetailsDto>> GetUserDetailsAsync(int id);
 }
