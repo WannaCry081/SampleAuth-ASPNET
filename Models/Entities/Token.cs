caef7b746cf5e9189f8fa2ee397fdd863f1b7cca
Ly <liraedata59@gmail.com>
2024-11-01 14:33:55 +0800

    chore: add 'IsRevoked' property in 'Token' entity

--- Models/Entities/Token.cs
+++ Models/Entities/Token.cs
@@ -9,6 +9,7 @@ public class Token : BaseEntity
     [ForeignKey("User")]
     public int UserId { get; init; }
     public string Refresh { get; set; } = null!;
+    public bool IsRevoked { get; set; } = false;
     public DateTime Expiration { get; init; }
 
     // Navigation Properties
