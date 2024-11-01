196920f9442c3000ec05f045b64927e4f623de1c
Ly <liraedata59@gmail.com>
2024-11-01 14:22:51 +0800

    chore: add 'AddTokenProperty' migration

--- Migrations/20241101061855_AddExpirationProperty.Designer.cs
+++ Migrations/20241101061855_AddExpirationProperty.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using sample_auth_aspnet.Data;
 
@@ -11,9 +12,11 @@ using sample_auth_aspnet.Data;
 namespace sample_auth_aspnet.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20241101061855_AddExpirationProperty")]
+    partial class AddExpirationProperty
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
