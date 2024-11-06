ee5473062a6ff41a3d668212500e18137f894ab1
Ly <liraedata59@gmail.com>
2024-11-07 00:48:10 +0800

    chore: add data validation in 'AuthLoginDto'

--- Models/Dtos/Auth/AuthLoginDto.cs
+++ Models/Dtos/Auth/AuthLoginDto.cs
@@ -1,3 +1,5 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace sample_auth_aspnet.Models.Dtos.Auth;
 
 /// <summary>
@@ -9,11 +11,15 @@ public class AuthLoginDto
     /// The email address of the user.
     /// </summary>
     /// <example>user@example.com</example>
+    [Required(ErrorMessage = "Email address is required.")]
+    [EmailAddress(ErrorMessage = "Invalid email address.")]
     public string Email { get; init; } = string.Empty;
 
     /// <summary>
     /// The password for the account.
     /// </summary>
     /// <example>StrongPassword123</example>
+    [Required(ErrorMessage = "Password is required.")]
+    [MaxLength(255, ErrorMessage = "Password cannot exceeds {1} characters.")]
     public string Password { get; init; } = string.Empty;
 };
