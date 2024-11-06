b2dfc0a759d25ad29b8e34441bf083561420787c
Ly <liraedata59@gmail.com>
2024-11-07 00:47:57 +0800

    chore: add data annotation validation in 'AuthRegisterDto'

--- Models/Dtos/Auth/AuthRegisterDto.cs
+++ Models/Dtos/Auth/AuthRegisterDto.cs
@@ -1,3 +1,5 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace sample_auth_aspnet.Models.Dtos.Auth;
 
 /// <summary>
@@ -9,29 +11,40 @@ public class AuthRegisterDto
     /// The email address of the user.
     /// </summary>
     /// <example>user@example.com</example>
+    [Required(ErrorMessage = "Email address is required.")]
+    [EmailAddress(ErrorMessage = "Invalid email address.")]
     public string Email { get; init; } = string.Empty;
 
     /// <summary>
     /// The first name of the user.
     /// </summary>
     /// <example>John</example>
+    [MaxLength(50, ErrorMessage = "First name cannot exceeds {1} characters.")]
     public string FirstName { get; init; } = string.Empty;
 
     /// <summary>
     /// The last name of the user.
     /// </summary>
     /// <example>Doe</example>
+    [MaxLength(50, ErrorMessage = "Last name cannot exceeds {1} characters.")]
     public string LastName { get; init; } = string.Empty;
 
     /// <summary>
     /// The password for the account.
     /// </summary>
     /// <example>StrongPassword123</example>
+    [Required(ErrorMessage = "Password is required.")]
+    [MaxLength(255, ErrorMessage = "Password cannot exceeds {1} characters.")]
+    [MinLength(8, ErrorMessage = "Password must be at least {1} characters.")]
+    [DataType(DataType.Password)]
     public string Password { get; init; } = string.Empty;
 
     /// <summary>
     /// The password confirmation.
     /// </summary>
     /// <example>StrongPassword123</example>
+    [Required(ErrorMessage = "RePassword is required.")]
+    [Compare("Password", ErrorMessage = "Passwords do not match.")]
+    [DataType(DataType.Password)]
     public string RePassword { get; init; } = string.Empty;
 }
