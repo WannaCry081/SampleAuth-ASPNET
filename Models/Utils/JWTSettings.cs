3614dd66cc97cf5c9fcb7623f8d8e0d73d9766a8
Ly <liraedata59@gmail.com>
2024-11-17 21:26:02 +0800

    ref: add 'ResetTokenExpiry' property

--- Models/Utils/JWTSettings.cs
+++ Models/Utils/JWTSettings.cs
@@ -5,6 +5,7 @@ public class JWTSettings
     public string Secret { get; set; } = string.Empty;
     public int RefreshTokenExpiry { get; set; }
     public int AccessTokenExpiry { get; set; }
+    public int ResetTokenExpiry { get; set; }
     public string Issuer { get; set; } = string.Empty;
     public string Audience { get; set; } = string.Empty;
 }
