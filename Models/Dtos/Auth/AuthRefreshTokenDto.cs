1c18f5c5e411e495bef629be822a924900aa0f4d
Ly <liraedata59@gmail.com>
2024-11-17 11:54:33 +0800

    docs: add xml comments in 'AuthRefreshTokenDto'

--- Models/Dtos/Auth/AuthRefreshTokenDto.cs
+++ Models/Dtos/Auth/AuthRefreshTokenDto.cs
@@ -2,8 +2,14 @@ using System.ComponentModel.DataAnnotations;
 
 namespace sample_auth_aspnet.Models.Dtos.Auth;
 
+/// <summary>
+///     DTO used for getting refresh tokens.
+/// </summary>
 public class AuthRefreshTokenDto
 {
+    /// <summary>
+    ///     The refresh token of the user.
+    /// </summary>
     [Required(ErrorMessage = "Refresh Token is required.")]
     public string Refresh { get; init; } = string.Empty;
 }
